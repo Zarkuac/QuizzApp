91d5cc93b9aee86ba5283df7a62eef59e0d4a407
ცოტნე ზარქუა <tsotne.zarkua@softgen.ge>
2024-11-03 22:02:18 +0400

    quiz-join page updated

--- src/app/pages/quiz-join/quiz-join.component.ts
+++ src/app/pages/quiz-join/quiz-join.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import {MatInputModule} from '@angular/material/input';
+import {MatButtonModule} from '@angular/material/button';
+import { FormsModule } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-quiz-join',
   standalone: true,
-  imports: [],
+  imports: [MatInputModule,MatButtonModule, FormsModule],
   templateUrl: './quiz-join.component.html',
   styleUrl: './quiz-join.component.css'
 })
 export class QuizJoinComponent {
+code!:string;
+name!:string;
 
+join(){
+  if(this.code && this.name){
+
+  } else {
+    // error
+  }
+}
 }
