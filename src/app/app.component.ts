9a6a39c0724eee8d1f28f13152ddd61f5ab741d6
ცოტნე ზარქუა <tsotne.zarkua@softgen.ge>
2024-11-03 19:36:53 +0400

    basic init, fake db.json

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {MatButtonModule} from '@angular/material/button';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,MatButtonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-test';
+  title: string = 'Task-Tracker';
 }
